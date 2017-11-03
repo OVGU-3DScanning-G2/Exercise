f80540fdee26cfdfd072d8267b34d5f8580501f1
Anaeijon <david.doering@knopf-und-wolle.de>
2017-11-03 13:15:53 +0100

    added relevant files for qmake Qt project

---
+++ include/MainWindow.h
@@ -0,0 +1,31 @@
+#ifndef MY_MAINWINDOW_H
+#define MY_MAINWINDOW_H
+
+#include <QtWidgets/QMainWindow>
+#include "GLwidget.h"
+#include "Point3d.h"
+
+#include "../Exercise_1/KDTree.h"
+
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+  public:
+    MainWindow();
+
+  private:
+    QMenu*    m_fileMenu;
+    QMenu*    m_viewMenu;
+    GLwidget* m_glWidget;
+
+  private:
+    void loadFileXYZ(const char* filename, std::vector<Point3d>& points);
+
+  private slots:
+    void openFile();
+    void changeProjection();
+};
+
+#endif
